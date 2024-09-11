75a62adc3077e8488b2a16f6f93b235a50c3e576
dnanunespy <dnanunes1@outlook.com>
2024-09-10 23:23:10 -0300

    04/09/2024 ajuste cadastro espacos 2 - Traducao para PT-BR

--- coworking_agendamento/settings.py
+++ coworking_agendamento/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reservas',  # Adicione esta linha
-    
+
 ]
 
 MIDDLEWARE = [
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
